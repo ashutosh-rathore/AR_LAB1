1608e4541c36b18fff06626881b0f03252e38a5f
Ashutosh Rathore <ashutoshrathore@Ashutoshs-MacBook-Air.local>
2021-06-12 23:09:55 +0530

    ARAzure

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.26"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+	features {}
+#it can be initial configuration data
+}
+
